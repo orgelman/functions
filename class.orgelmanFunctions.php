8fb21352fc22cc8513fd5df9bab579fa774c4d7f
Tobias Orgelman Jonson <GitHub@toube.se>
2017-02-01 17:41:40 +0100

    Update class.orgelmanFunctions.php

--- class.orgelmanFunctions.php
+++ class.orgelmanFunctions.php
@@ -181,14 +181,7 @@ return $count_size;
 
     return $zip->close();
 }
-   public function obfuscate_email($email)
-{
-    $em   = explode("@",$email);
-    $name = implode(array_slice($em, 0, count($em)-1), '@');
-    $len  = floor(strlen($name)/2);
-
-    return substr($name,0, $len) . str_repeat('*', $len) . "@" . end($em);
-}
+   
    public function folderSize($dir){
 $count_size = 0;
 $count = 0;

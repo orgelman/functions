bd427cbf636f5bd8e4ae2b9e784daf64ec9bf3cc
Tobias Orgelman Jonson <GitHub@toube.se>
2017-04-15 19:26:21 +0200

    Update missing_jquery.js

--- template/missing_jquery.js
+++ template/missing_jquery.js
@@ -1,16 +1,16 @@
 var jQueryScriptOutputted = false;
 initJQuery();
 function initJQuery() {
-   var eurl = "js/ajax/error.php";
+   var eurl = "ajax/error.php";
    if (typeof(jQuery) == "undefined") {
       console.log("jQuery undefined");
       if(!jQueryScriptOutputted) {
          jQueryScriptOutputted = true;
          
-         var script  = document.createElement('script');
-         script.src  = '//code.jquery.com/jquery-3.2.0.min.js';
-         script.type = 'text/javascript';
-         document.getElementsByTagName('head')[0].appendChild(script);
+         var script  = document.createElement("script");
+         script.src  = "//code.jquery.com/jquery-3.2.0.min.js";
+         script.type = "text/javascript";
+         document.getElementsByTagName("head")[0].appendChild(script);
       }
       setTimeout(initJQuery, 50);
    } else {
